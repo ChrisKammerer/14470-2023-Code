549ab19bfe60c1aab4578873b5a410b0903b6313
ChrisKammerer <ckamm001@outlook.com>
2022-10-28 09:00:03 -0400

    Meet 1 code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/TeleOpFieldCentric.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/TeleOpFieldCentric.java
@@ -5,19 +5,12 @@ import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-/**
- * This opmode demonstrates how one would implement field centric control using
- * `SampleMecanumDrive.java`. This file is essentially just `TeleOpDrive.java` with the addition of
- * field centric control. To achieve field centric control, the only modification one needs is to
- * rotate the input vector by the current heading before passing it into the inverse kinematics.
- * <p>
- * See lines 42-57.
- */
 @TeleOp(group = "drive")
 public class TeleOpFieldCentric extends LinearOpMode {
 
@@ -25,9 +18,12 @@ public class TeleOpFieldCentric extends LinearOpMode {
     DcMotor rightWinch;
     DcMotor turret;
     DcMotor chainBar;
+    Servo leftIntake;
+    Servo rightIntake;
+
     double subtractHeading = 0;
-    final double CHAIN_BAR_DELAY_TIME = 0.0;
-    private ElapsedTime liftTimer = new ElapsedTime();
+    //final double CHAIN_BAR_DELAY_TIME = 0.0;
+    //private ElapsedTime liftTimer = new ElapsedTime();
 
     public enum LiftState {
         LIFT_START,
@@ -36,13 +32,21 @@ public class TeleOpFieldCentric extends LinearOpMode {
     }
 
     public enum ArmState {
-        ARM_RAISE,
-        ARM_LOWER
+        ARM_UPPER,
+        ARM_LOWER,
+        ARM_MIDDLE,
+        ARM_REVERSE
     }
 
     LiftState liftState = LiftState.LIFT_START;
+    //ArmState armState = ArmState.ARM_LOWER;
+
     final int LIFT_LOW = 0;
-    final int LIFT_HIGH = 3500;
+    final int LIFT_HIGH = 4050;
+    final int ARM_LOW = 0;
+    final int ARM_MID = 38;
+    final int ARM_HIGH = 82;
+    final int ARM_BACK = -200;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -50,22 +54,26 @@ public class TeleOpFieldCentric extends LinearOpMode {
         rightWinch = hardwareMap.dcMotor.get("rightWinch");
         turret = hardwareMap.dcMotor.get("turret");
         chainBar = hardwareMap.dcMotor.get("chainBar");
+        leftIntake = hardwareMap.servo.get("leftIntake");
+        rightIntake = hardwareMap.servo.get("rightIntake");
+        leftIntake.setPosition(.55);
+        rightIntake.setPosition(0);
 
         leftWinch.setDirection(DcMotor.Direction.REVERSE);
+        chainBar.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         leftWinch.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         rightWinch.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         leftWinch.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rightWinch.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rightWinch.setTargetPosition(LIFT_LOW);
         leftWinch.setTargetPosition(LIFT_LOW);
+//        chainBar.setTargetPosition(ARM_LOW);
         rightWinch.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         leftWinch.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+//        chainBar.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-        chainBar.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         chainBar.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
-
-        liftTimer.reset();
+        //liftTimer.reset();
 
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
         // We want to turn off velocity control for teleop
@@ -82,13 +90,22 @@ public class TeleOpFieldCentric extends LinearOpMode {
         while (opModeIsActive() && !isStopRequested()) {
             switch (liftState) {
                 case LIFT_START:
-                    if (gamepad1.y) {
+                    leftWinch.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                    rightWinch.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                    leftWinch.setPower(.05);
+                    rightWinch.setPower(.05);
+                    if (gamepad2.y) {
+                        leftWinch.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                        chainBar.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
                         leftWinch.setTargetPosition(LIFT_HIGH);
                         rightWinch.setTargetPosition(LIFT_HIGH);
-                        leftWinch.setPower(.55);
-                        rightWinch.setPower(-.55);
+                        rightWinch.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                        leftWinch.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                        leftWinch.setPower(.75);
+                        rightWinch.setPower(.75);
                         liftState = LiftState.LIFT_EXTEND;
                     }
+
                     break;
                 case LIFT_EXTEND:
                     if (Math.abs(leftWinch.getCurrentPosition()-LIFT_HIGH)<5
@@ -97,32 +114,55 @@ public class TeleOpFieldCentric extends LinearOpMode {
                     }
                     break;
                 case LIFT_RETRACT:
-                    if(gamepad1.a){
+                    if(gamepad2.a){
                         leftWinch.setTargetPosition(LIFT_LOW);
                         rightWinch.setTargetPosition(LIFT_LOW);
-                        leftWinch.setPower(-.55);
-                        rightWinch.setPower(.55);
-                        liftState = LiftState.LIFT_START;
+                        leftWinch.setPower(-.75);
+                        rightWinch.setPower(.75);
+                        if(Math.abs(leftWinch.getCurrentPosition()-LIFT_LOW)<5 || Math.abs(rightWinch.getCurrentPosition()-LIFT_LOW)<5)
+                            liftState = LiftState.LIFT_START;
                     }
+
                     break;
                 default:
                     liftState = LiftState.LIFT_START;
             }
-            // rotate turret
-            if(gamepad1.dpad_left)
-                turret.setPower(.5);
-            else if(gamepad1.dpad_right)
-                turret.setPower(-.5);
-            else turret.setPower(0);
-            // actuate chainbar
-            if(gamepad1.x)
-                chainBar.setPower(.7);
-            else if(gamepad1.b)
-                chainBar.setPower(-.7);
-            else chainBar.setPower(0);
 
+            // rotate turret
+            if(gamepad2.dpad_left)
+                turret.setPower(.6);
+            else if(gamepad2.dpad_right)
+                turret.setPower(-.6);
+            else turret.setPower(0);
+
+            //intake
+            if(gamepad2.left_bumper) {
+                leftIntake.setPosition(0);
+                rightIntake.setPosition(.55);
+            }
+            else if(gamepad2.right_bumper) {
+                leftIntake.setPosition(.55);
+                rightIntake.setPosition(0);
+            }
+
+            //chain bar
+//            if(gamepad2.dpad_up){
+//                chainBar.setTargetPosition(ARM_HIGH);
+//                chainBar.setPower(1);
+//            }
+//            if(gamepad2.dpad_right){
+//                chainBar.setTargetPosition(ARM_MID);
+//                chainBar.setPower(1);
+//            }
+//            if(gamepad2.dpad_down){
+//                chainBar.setTargetPosition(ARM_LOW);
+//                chainBar.setPower(1);
+//            }
+            chainBar.setPower(gamepad1.right_trigger-.05);
+            //reset FOD
             if (gamepad1.right_stick_button)
                 subtractHeading = drive.getPoseEstimate().getHeading();
+
             // Read pose
             Pose2d poseEstimate = drive.getPoseEstimate();
             // Create a vector from the gamepad x/y inputs
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/autos/RedLeftAuto.java
@@ -0,0 +1,137 @@
+package org.firstinspires.ftc.teamcode.drive.opmode.autos;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.apriltag.AprilTagDetection;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvInternalCamera;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import java.util.ArrayList;
+
+@Autonomous
+public class RedLeftAuto extends LinearOpMode{
+    OpenCvCamera camera;
+    AprilTagDetectionPipeline aprilTagDetectionPipeline;
+
+    static final double FEET_PER_METER = 3.28084;
+
+    // Lens intrinsics
+    // UNITS ARE PIXELS
+    // NOTE: this calibration is for the C920 webcam at 800x448.
+    // You will need to do your own calibration for other configurations!
+    double fx = 578.272;
+    double fy = 578.272;
+    double cx = 402.145;
+    double cy = 221.506;
+
+    // UNITS ARE METERS
+    double tagsize = 0.166;
+
+    // Tag ID 1,2,3 from the 36h11 family
+    int LEFT = 0;
+    int MIDDLE = 1;
+    int RIGHT = 2;
+    AprilTagDetection tagOfInterest = null;
+
+    @Override
+    public void runOpMode()
+    {
+
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        aprilTagDetectionPipeline = new AprilTagDetectionPipeline(tagsize, fx, fy, cx, cy);
+
+        camera.setPipeline(aprilTagDetectionPipeline);
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                camera.startStreaming(800,448, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode)
+            {
+
+            }
+        });
+
+        telemetry.setMsTransmissionInterval(50);
+
+        /*
+         * The INIT-loop:
+         * This REPLACES waitForStart!
+         */
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        Pose2d startPose = new Pose2d(0,0,0);
+        while (!isStarted() && !isStopRequested())
+        {
+            ArrayList<AprilTagDetection> currentDetections = aprilTagDetectionPipeline.getLatestDetections();
+            if(currentDetections.size() != 0)
+            {
+                boolean tagFound = false;
+                for(AprilTagDetection tag : currentDetections)
+                {
+                    if(tag.id == LEFT || tag.id == MIDDLE || tag.id == RIGHT)
+                    {
+                        tagOfInterest = tag;
+                        tagFound = true;
+                        break;
+                    }
+                }
+
+                if(tagFound)
+                {
+                    telemetry.addData("Tag found", tagOfInterest.id);
+                }
+                else
+                {
+                    telemetry.addLine("Tag not found");
+                }
+
+            }
+            telemetry.update();
+            sleep(20);
+        }
+
+
+        /* Actually do something useful */
+        Trajectory traj1 = drive.trajectoryBuilder(startPose)
+                .lineTo(new Vector2d(26, 0)).build();
+        Trajectory traj2 = drive.trajectoryBuilder(traj1.end())
+                .lineTo(new Vector2d(26, -10)).build();
+        double parkY = 0;
+        if(tagOfInterest==null || tagOfInterest.id == LEFT){
+            parkY = 25;
+        }else if(tagOfInterest.id==MIDDLE){
+            parkY = 0;
+        }else if(tagOfInterest.id == RIGHT){
+            parkY = -21;
+        }
+        Trajectory traj3 = drive.trajectoryBuilder(traj2.end())
+                .lineTo(new Vector2d(26, parkY)).build();
+        drive.followTrajectory(traj1);
+        drive.followTrajectory(traj2);
+        drive.followTrajectory(traj3);
+
+        /* You wouldn't have this in your autonomous, this is just to prevent the sample from ending */
+
+    }
+
+}
